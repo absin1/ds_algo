443c82f542a5d5476ba8f3db5c92674336824bad
Abhinav <abhinav1@istarindia.com>
2017-12-18 21:05:11 +0530

    Meter Down

--- src/thirtydaysofcode/Solution10.java
+++ src/thirtydaysofcode/Solution10.java
@@ -3,7 +3,7 @@ package thirtydaysofcode;
 import java.util.Scanner;
 
 public class Solution10 {
-
+	//524283 >> 16
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
