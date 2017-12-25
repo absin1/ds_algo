0feb0568c9d7c4334083c664aaf3db6926a15f9b
Abhinav <abhinav1@istarindia.com>
2017-12-25 14:05:12 +0530

    Stuck on Big Number Factor Project Euler

--- src/projectEuler/Problem3.java
+++ src/projectEuler/Problem3.java
@@ -3,6 +3,8 @@
  */
 package projectEuler;
 
+import java.util.Arrays;
+
 /**
  * @author absin
  * 
@@ -14,9 +16,36 @@ public class Problem3 {
 	public static void main(String args[]) {
 		Double N = 600851475143.00;
 		double sqrt = Math.floor(Math.sqrt(N));
-		boolean[] isPrime = new boolean[(int) sqrt];
-		for (double i = 2; i <= sqrt; i++) {
-			
+		double arrayRowSize = Math.ceil((N / ((double) Integer.MAX_VALUE)));
+		// System.out.println(arrayRowSize+"<"+Integer.MAX_VALUE);
+		// System.out.println(arrayRowSize*(double) Integer.MAX_VALUE+">"+N);
+		boolean[][] isPrime = new boolean[(int) arrayRowSize][Integer.MAX_VALUE];
+		Arrays.fill(isPrime, Boolean.TRUE);
+		isPrime[0][0] = false; // 1 is neither prime nor composite
+		for (double i = 0; i < arrayRowSize; i++) {
+			for (double j = 0; j < Integer.MAX_VALUE; j++) {
+				double factor = (i * Integer.MAX_VALUE) + (j + 1);
+				for (double k = i; k < arrayRowSize; k++) {
+					for (double l = 0; l < Integer.MAX_VALUE; l++) {
+						double num = (k * Integer.MAX_VALUE) + (l + 1);
+						if ((num <= N) && isPrime[(int) k][(int) l] && (num % factor == 0)) {
+							isPrime[(int) k][(int) l] = false;
+						}
+					}
+				}
+			}
+		}
+		double maxFactor = 1;
+		for (double i = 0; i < arrayRowSize; i++) {
+			for (double j = 0; j < Integer.MAX_VALUE; j++) {
+				double num = (i * Integer.MAX_VALUE) + (j + 1);
+				if (num < N) {
+					if (N % num == 0) {
+						if (num > maxFactor)
+							maxFactor = num;
+					}
+				}
+			}
 		}
 	}
 }
