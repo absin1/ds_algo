26b542a91d38ea4f5232012c3e05599501317964
Abhinav <abhinav1@istarindia.com>
2017-12-25 14:49:45 +0530

    Stuck in the Middle with You

--- src/projectEuler/Problem3.java
+++ src/projectEuler/Problem3.java
@@ -4,6 +4,7 @@
 package projectEuler;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @author absin
@@ -15,37 +16,107 @@ import java.util.Arrays;
 public class Problem3 {
 	public static void main(String args[]) {
 		Double N = 600851475143.00;
-		double sqrt = Math.floor(Math.sqrt(N));
-		double arrayRowSize = Math.ceil((N / ((double) Integer.MAX_VALUE)));
-		// System.out.println(arrayRowSize+"<"+Integer.MAX_VALUE);
-		// System.out.println(arrayRowSize*(double) Integer.MAX_VALUE+">"+N);
-		boolean[][] isPrime = new boolean[(int) arrayRowSize][Integer.MAX_VALUE];
-		Arrays.fill(isPrime, Boolean.TRUE);
-		isPrime[0][0] = false; // 1 is neither prime nor composite
-		for (double i = 0; i < arrayRowSize; i++) {
-			for (double j = 0; j < Integer.MAX_VALUE; j++) {
-				double factor = (i * Integer.MAX_VALUE) + (j + 1);
-				for (double k = i; k < arrayRowSize; k++) {
-					for (double l = 0; l < Integer.MAX_VALUE; l++) {
-						double num = (k * Integer.MAX_VALUE) + (l + 1);
-						if ((num <= N) && isPrime[(int) k][(int) l] && (num % factor == 0)) {
-							isPrime[(int) k][(int) l] = false;
+		squareRootandPrimeStatusCapitalization(N);
+		// squareRootPrincipleCapitalization(N);
+		// bruteForcing(N);
+	}
+
+	private static void squareRootandPrimeStatusCapitalization(Double N) {
+		/*
+		 * Even with square root capitalization we observe that we are having to
+		 * check the primability again and again. This can be ghastly
+		 * time-taking. Let's use a collection to manage the prime status.
+		 */
+		HashSet<Double> primes = new HashSet<>();
+		double maxPrimeDivisor = 1;
+		for (double i = 2; i <= Math.floor((Math.sqrt(N))); i++) {
+			if (N % i == 0) // it is a divisor
+			{
+				double firstFactor = i;
+				double secondFactor = N / i;
+				if (primes.contains(firstFactor)) {
+					if (firstFactor > maxPrimeDivisor) {
+						maxPrimeDivisor = firstFactor;
+					}
+				} else {
+					if (isPrime(firstFactor)) { // The first factor
+						primes.add(firstFactor);
+						if (firstFactor > maxPrimeDivisor) {
+							maxPrimeDivisor = firstFactor;
+						}
+					}
+				}
+				if (primes.contains(secondFactor)) {
+					if (secondFactor > maxPrimeDivisor) {
+						maxPrimeDivisor = secondFactor;
+					}
+				} else {
+
+					if (isPrime(secondFactor)) { // The associated pair
+						if (secondFactor > maxPrimeDivisor) {
+							maxPrimeDivisor = secondFactor;
 						}
 					}
 				}
 			}
 		}
-		double maxFactor = 1;
-		for (double i = 0; i < arrayRowSize; i++) {
-			for (double j = 0; j < Integer.MAX_VALUE; j++) {
-				double num = (i * Integer.MAX_VALUE) + (j + 1);
-				if (num < N) {
-					if (N % num == 0) {
-						if (num > maxFactor)
-							maxFactor = num;
+		System.out.println(maxPrimeDivisor);
+	}
+
+	private static void squareRootPrincipleCapitalization(Double N) {
+		/*
+		 * Lets take 20, now the factors are 2, 5 and 10. So if we go till say 4
+		 * (which is the floor of the square root of 20, we can find all the
+		 * pairs. Let's capitalize on this pattern.
+		 */
+		double maxPrimeDivisor = 1;
+		for (double i = 2; i <= Math.floor((Math.sqrt(N))); i++) {
+			if (N % i == 0) // it is a divisor
+			{
+				double firstFactor = i;
+				double secondFactor = N / i;
+				if (isPrime(firstFactor)) { // The first factor
+					if (firstFactor > maxPrimeDivisor) {
+						maxPrimeDivisor = firstFactor;
+					}
+				}
+				if (isPrime(secondFactor)) { // The associated pair
+					if (secondFactor > maxPrimeDivisor) {
+						maxPrimeDivisor = secondFactor;
+					}
+				}
+			}
+		}
+		System.out.println(maxPrimeDivisor);
+	}
+
+	private static void bruteForcing(Double N) {
+		/*
+		 * This may take forever depending on your system, I don't have that
+		 * long!!
+		 */
+		double maxPrimeDivisor = 1;
+		for (double i = 2; i < N; i++) {
+			if (N % i == 0) // it is a divisor
+			{
+				if (isPrime(i)) {
+					if (i > maxPrimeDivisor) {
+						maxPrimeDivisor = i;
 					}
 				}
 			}
 		}
 	}
+
+	private static boolean isPrime(double N) {
+		boolean boo = true;
+		for (double i = 2; i < N; i++) {
+			if (N % i == 0) {
+				boo = false;
+				break;
+			}
+		}
+		return boo;
+	}
+
 }
